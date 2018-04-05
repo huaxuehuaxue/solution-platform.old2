ab131e3a43fa88c0a56615add85b23f79f4d756b
chendoudou <hua-xue21@163.com>
2018-04-05 22:26:12 +0800

    first

---
+++ sp-nodejs-server/src/main/java/com/h3c/solution/platform/spnodejsserver/SpNodejsServerApplication.java
@@ -0,0 +1,12 @@
+package com.h3c.solution.platform.spnodejsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpNodejsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpNodejsServerApplication.class, args);
+	}
+}
